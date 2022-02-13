9a4f999238b8aa4609e61d2463904bbccaba5bec
alfism1 <alfialfarisi@gmail.com>
2022-02-13 16:26:16 +0700

    Update changeicon

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700,800&display=swap"
                         rel="stylesheet"
                     />
-                    <link rel="icon" type="image/png" href="/images/favicon.png"></link>
+                    <link rel="icon" type="image/png" href="/images/favicon.ico"></link>
                 </Head>
                 <body>
                     <Main />
